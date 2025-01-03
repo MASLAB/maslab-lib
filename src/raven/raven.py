0a4f466558bc3cdfaeac30dd9cfcd13e36df4da0
Quang Kieu <quang.p.kieu@gmail.com>
2025-01-03 15:26:55 -0500

    Restructuring message format and handling

--- src/raven/raven.py
+++ src/raven/raven.py
@@ -39,6 +39,13 @@ class Raven:
         ENCODER_VALUE = 7
         ERROR = 8
 
+    # Read write option
+    @unique
+    class __ReadWrite(Enum):
+        # First bit of message type
+        WRITE = 0x00
+        READ = 0x80
+
     class __RavenSerial:
         """
         /***
@@ -58,10 +65,12 @@ class Raven:
         */
         """
 
-        __START = b"\xAA"
-        __ACK = 0x01
-        __REPLY = 0x02
-        __NUM_HDR = 9
+        __START = 0xAA
+
+        @unique
+        class ReadWrite(Enum):
+            WRITE = 0
+            READ = 1
 
         class CRC8Encoder:
             SMBUS_POLY = 0x07
@@ -91,20 +100,11 @@ class Raven:
             # Read serial data and return (header, message)
             crc = 0
             message = bytes()
-            header = 0
             data = self.__serial.read(1)
 
             # Start
             if data == self.__START:
                 crc = self.__crc.crc(data)
-                data = self.__serial.read(1)
-            else:
-                return None, None
-
-            # Header
-            if len(data) == 1 and data[0] < self.__NUM_HDR:
-                crc = self.__crc.crc(data, crc)
-                header = data[0]
                 data = self.__serial.read(1)
             else:
                 return None, None
@@ -118,9 +118,7 @@ class Raven:
                 return None, None
 
             # Data:
-            if len(data) == 0:
-                data = self.__serial.read(1)
-            elif len(data) == length:
+            if len(data) == length:
                 crc = self.__crc.crc(data, crc)
                 message = data
                 data = self.__serial.read(1)
@@ -131,44 +129,27 @@ class Raven:
             if len(data) == 1:
                 crc = self.__crc.crc(data, crc)
                 if crc == 0:
-                    return header, message
+                    return message[0], message[1:]
             return None, None
 
-        def __make_message(self, header, rw, data):
-            rw_header = header.value
-            if rw:
-                rw_header |= 0x80
-            message = self.__START + bytes([rw_header, len(data)]) + data
+        def __make_message(self, data):
+            message = self.__START + bytes([len(data), data]) + data
             crc = self.__crc.crc(message)
             message = message + bytes([crc])
             return message, crc
 
-        def write_value(self, header, data, retry=0):
+        def send(self, data, retry=0):
             self.__serial.reset_input_buffer()
-            message, crc = self.__make_message(header, 1, data)
+            message, crc = self.__make_message(data)
             self.__serial.write(message)
 
-            header, received_crc = self.__read()
-            if header and header == self.__ACK:
-                if received_crc == crc.to_bytes():
-                    return True
+            received_crc, data = self.__read()
+            if received_crc == crc.to_bytes():
+                return data
             if retry > 0:
-                return self.write_value(header, data, retry - 1)
+                return self.send(data, retry - 1)
             else:
-                return False
-
-        def read_value(self, header, data, retry=0):
-            self.__serial.reset_input_buffer()
-            message, __ = self.__make_message(header, 0, data)
-            self.__serial.write(message)
-
-            header, received_message = self.__read()
-            if header and header == self.__REPLY:
-                return received_message
-            if retry > 0:
-                return self.read_value(header, data, retry - 1)
-            else:
-                return False
+                return None
 
     def __init__(self, port: str = None, timeout: float = 0.001):
         """
@@ -181,6 +162,19 @@ class Raven:
         # Raven has fixed baud at 460800
         self.__serial = Raven.__RavenSerial(port, 460800, timeout)
 
+    @staticmethod
+    def __make_message(message_type: __MessageType, rw: __ReadWrite, data):
+        header_byte = bytes([message_type.value + rw.value])
+        return header_byte + data
+
+    def __read_value(self, message_type: __MessageType, data, retry=0):
+        message = Raven.__make_message(message_type, Raven.__ReadWrite.READ, data)
+        return self.__serial.send(message, retry)
+
+    def __write_value(self, message_type: __MessageType, data, retry=0):
+        message = Raven.__make_message(message_type, Raven.__ReadWrite.WRITE, data)
+        return self.__serial.send(message, retry) is not None
+
     def get_motor_mode(self, motor_channel: MotorChannel, retry):
         """
         Get motor mode
@@ -188,7 +182,7 @@ class Raven:
         @return: Raven.MotorMode or None if fails
         """
         assert type(motor_channel) == Raven.MotorChannel
-        value = self.__serial.read_value(
+        value = self.__read_value(
             Raven.__MessageType.MOTOR_MODE, motor_channel.value, retry
         )
         if value and len(value) == 1:
@@ -211,7 +205,7 @@ class Raven:
         @return: True if success
         """
         assert type(motor_channel) == Raven.MotorChannel
-        return self.__serial.write_value(
+        return self.__write_value(
             Raven.__MessageType.MOTOR_MODE,
             motor_channel.value + motor_mode.value,
             retry,
@@ -225,7 +219,7 @@ class Raven:
         @return: command (rps if motor mode is speed and revolutions if motor mode is position) or None if fails
         """
         assert type(motor_channel) == Raven.MotorChannel
-        value = self.__serial.read_value(
+        value = self.__read_value(
             Raven.__MessageType.MOTOR_CMD, motor_channel.value, retry
         )
         if value and len(value) == 4:
@@ -241,7 +235,7 @@ class Raven:
         @return: True if success
         """
         assert type(motor_channel) == Raven.MotorChannel
-        return self.__serial.write_value(
+        return self.__write_value(
             Raven.__MessageType.MOTOR_CMD,
             motor_channel.value + struct.pack("f", value),
             retry,
@@ -255,7 +249,7 @@ class Raven:
         @return: (P,I,D) values or None if fails
         """
         assert type(motor_channel) == Raven.MotorChannel
-        value = self.__serial.read_value(
+        value = self.__read_value(
             Raven.__MessageType.MOTOR_PID, motor_channel.value, retry
         )
         if value and len(value) == 12:
@@ -279,7 +273,7 @@ class Raven:
         @retry: number of retries if command fails
         @return: True if success
         """
-        return self.__serial.write_value(
+        return self.__write_value(
             Raven.__MessageType.MOTOR_PID,
             motor_channel.value + struct.pack("fff", p_gain, i_gain, d_gain),
             retry,
@@ -307,7 +301,7 @@ class Raven:
         if degree < -90 or degree > 90:
             return ValueError("Invalid degree")
 
-        return self.__serial.write_value(
+        return self.__write_value(
             Raven.__MessageType.SERVO_VALUE,
             servo_channel.value + struct.pack("H", Raven.__deg_to_count(degree)),
             retry,
@@ -321,7 +315,7 @@ class Raven:
         @return: number of encoder count or None if fails
         """
         assert type(motor_channel) == Raven.MotorChannel
-        value = self.__serial.read_value(
+        value = self.__read_value(
             Raven.__MessageType.ENCODER_VALUE, motor_channel.value, retry
         )
         if value and len(value) == 4:
